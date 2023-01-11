68e867fa1ea4da188027a98c5e44863738358bb4
unknown <hasnainpriam925@gmail.com>
2023-01-11 17:31:31 +0600

    Navbar Footer Layout added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Header from './components/Header/Header'
+import Main from './components/Layout/Main';
+import Header from './components/Header/Header';
+import Error from './components/Error/Error';
+import BigGallery from './components/Gallery/BigGallery';
+import Pricing from './components/Pricing/Pricing'
+import Blog from './components/Blog/Blog'
+import About from './components/About/About';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import { ToastContainer } from 'react-toastify';
+import CheckOut from './components/Checkout/Checkout';
+
 
 function App() {
 
-  const router=createBrowserRouter([
+const router=  createBrowserRouter([
 
-
-    {
-      path:'/', element: <Header></Header>
-    }
+  {
+    path:'/',
+    element: <Main></Main>,
+    children:[
+      {path:'/', element:<Header></Header>},
+      {path:'/home', element:<Header></Header>},
+      {path:'/bigallery' ,element:<BigGallery></BigGallery> },
+      {path:'/pricing', element:<Pricing></Pricing>},
+      {path:'/blog', element:<Blog></Blog>},
+      {path:'/about', element:<About></About>},
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+      {
+        path: '/checkout',
+        element: <CheckOut></CheckOut>
+      },
+      {path:'*', element: <Error></Error>},
+    ]
+  }
 
   ])
+
 
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
+      <ToastContainer />
+
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+import logo from '../../Assets/icons8-camera-66.png'
+
+const Navbar = () => {
+   
+
+    return (
+        <header className="flex justify-between items-center py-8 md:py-8  max-w-screen-2xl px-4 md:px-10 mx-auto">
+
+            <Link to='/' className="text-gray-600 inline-flex items-center text-black-800 text-2xl md:text-3xl font-bold gap-2.5" aria-label="logo">
+                <img src={logo} alt="" height={40} width={40} />
+
+                Prime Studio
+            </Link>
+
+            <nav className="hidden lg:flex gap-12">
+                <Link to='/' className="text-indigo-500 text-lg font-semibold">Home</Link>
+                <Link to='/bigallery' className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">Gallery</Link>
+                <Link to='/pricing' className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">Pricing</Link>
+                <Link to='/blog' className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">Blogs</Link>
+                <Link to='/about' className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">About</Link>
+
+               
+            </nav>
+
+            <Link to='/pricing' className="hidden lg:inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Hire Now</Link>
+
+            <button type="button" className="inline-flex items-center lg:hidden bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold rounded-lg gap-2 px-2.5 py-2">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+                </svg>
+
+                Menu
+            </button>
+
+        </header>
+
+    );
+};
+
+export default Navbar;
